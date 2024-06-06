1f7cc32891bf7f19deda0039f4104d0cff7e919f
Yves Antônio <yves@gec.inatel.br>
2024-06-06 16:18:14 -0300

    feat: estilização da home e componente de card café

---
+++ src/components/CoffeeCard/styles.ts
@@ -0,0 +1,131 @@
+import { styled } from 'styled-components'
+import { texts } from '../../styles/texts'
+
+export const CoffeeCardContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  border-radius: 6px 36px;
+  width: 256px;
+  padding: 0 20px 20px;
+
+  justify-content: center;
+  text-align: center;
+
+  background: ${(props) => props.theme['base-card']};
+`
+
+export const CoffeImg = styled.img`
+  max-width: 120px;
+  max-height: 120px;
+  align-self: center;
+  margin-top: -20px;
+`
+
+export const Tags = styled.div`
+  display: flex;
+  margin-top: 12px;
+  gap: 4px;
+  align-self: center;
+  align-items: center;
+
+  span {
+    ${texts.fonts.tag}
+    color: ${(props) => props.theme['yellow-dark']};
+    background: ${(props) => props.theme['yellow-light']};
+    padding: 4px 8px;
+    border-radius: 6px;
+  }
+`
+
+export const Title = styled.span`
+  margin-top: 16px;
+
+  ${texts.fonts.titleS}
+  color: ${(props) => props.theme['base-subtitle']};
+`
+
+export const Description = styled.span`
+  margin-top: 8px;
+
+  ${texts.fonts.textS}
+  color: ${(props) => props.theme['base-label']};
+`
+
+export const Details = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 32px;
+`
+
+export const Price = styled.div`
+  display: flex;
+  align-items: baseline;
+  gap: 2px;
+
+  color: ${(props) => props.theme['base-text']};
+
+  span:first-child {
+    ${texts.fonts.textS}
+  }
+
+  span:last-child {
+    ${texts.fonts.titleM}
+  }
+`
+
+interface OrderProps {
+  itemAdded?: boolean
+}
+
+export const Order = styled.div<OrderProps>`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  > button {
+    display: flex;
+    background-color: ${(props) => props.theme['purple-dark']};
+    transition: background-color 0.2s;
+    border-radius: 6px;
+    padding: 8px;
+
+    border: 0;
+
+    &:hover {
+      background-color: ${(props) => props.theme.purple};
+    }
+  }
+`
+
+export const QuantityInput = styled.div`
+  display: flex;
+  gap: 8px;
+
+  padding: 8px;
+  background-color: ${(props) => props.theme['base-button']};
+  border-radius: 6px;
+
+  button {
+    background-color: transparent;
+    display: flex;
+    align-items: center;
+    border: 0;
+  }
+
+  button svg {
+    color: ${(props) => props.theme.purple};
+
+    transition: all 0.2s;
+
+    &:hover {
+      color: ${(props) => props.theme['purple-dark']};
+    }
+  }
+
+  span {
+    padding-top: 2px;
+    color: ${(props) => props.theme['base-title']};
+  }
+`
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,79 @@
+// import { Timer, ShoppingCart, Package, Coffee } from 'phosphor-react'
+import heroImg from '../../assets/hero_img.svg'
+import {
+  CoffeeList,
+  HeroContainer,
+  HeroHead,
+  HeroInfo,
+  HeroPros,
+} from './styles'
+import Icon from '../../assets/icons/Icon.png'
+import Icon1 from '../../assets/icons/Icon-1.png'
+import Icon2 from '../../assets/icons/Icon-2.png'
+import Icon3 from '../../assets/icons/Icon-3.png'
+import { CoffeeCard } from '../../components/CoffeeCard'
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <HeroContainer>
+        <HeroInfo>
+          <HeroHead>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffe Delivery você recebe seu café onde estiver, a qualquer
+              hora
+            </p>
+          </HeroHead>
+          <HeroPros>
+            <div>
+              <div>
+                {/* <ShoppingCart
+                size={16}
+                weight="fill"
+                color="#C47F17"
+                style={{ backgroundColor: '#F1E9C9' }}
+              /> */}
+                <img src={Icon} alt="" />
+              </div>
+              <p>Compra simples e segura</p>
+            </div>
+
+            <div>
+              {/* <Package size={16} weight="fill" /> */}
+              <img src={Icon2} alt="" />
+              <p>Embalagem mantém o café intacto</p>
+            </div>
+
+            <div>
+              {/* <Timer size={16} weight="fill" /> */}
+              <img src={Icon1} alt="" />
+              <p>Entrega rápida e rastreada</p>
+            </div>
+
+            <div>
+              {/* <Coffee size={16} weight="fill" /> */}
+              <img src={Icon3} alt="" />
+              <p>O café chega fresquinho até você</p>
+            </div>
+          </HeroPros>
+        </HeroInfo>
+        <img src={heroImg} alt="" />
+      </HeroContainer>
+      <CoffeeList>
+        <h1>Nossos cafés</h1>
+
+        <div>
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+          <CoffeeCard />
+        </div>
+      </CoffeeList>
+    </>
   )
 }
